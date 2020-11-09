abac5897b91be8e11df65c14d38d2b3a4b2876f4
architaharathi <archita.hi@gmail.com>
2020-11-08 23:17:31 -0800

    creating initial predictor file

---
+++ target_following/scripts/predictor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+
+# PREDICTOR.PY 
+# class to predict robot movement
+
+FREQ = 10 # Hz
+SLEEP = 2
+
+class Predictor:
+	def __init__(self):
+		
+
+	
