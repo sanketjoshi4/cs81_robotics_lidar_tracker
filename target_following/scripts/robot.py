c8e07438cb635c7357151d90b244b1f46b353d8e
Sanket Joshi <sanketjoshi4@gmail.com>
2020-11-12 20:11:15 -0800

    ID : Bug fixes and accuracy improvement

--- target_following/scripts/robot.py
+++ target_following/scripts/robot.py
@@ -23,6 +23,11 @@ LASER_RANGE = 20.0
 BASE_LINEAR_VELOCITY = 1.0
 BASE_ANGULAR_VELOCITY = PI / 2
 
+START_X_MAP = 3.0
+START_Y_MAP = 5.0
+
+
+# START_Z_MAP = 0.0
 
 class Robot:
     def __init__(self):
@@ -31,28 +36,26 @@ class Robot:
         self.world_sub = rospy.Subscriber("map", OccupancyGrid, self.world_callback, queue_size=1)
         self.odom_sub = rospy.Subscriber("robot_0/odom", Odometry, self.odom_callback)
         self.sub_laser = rospy.Subscriber("robot_0/base_scan", LaserScan, self.laser_scan_callback, queue_size=1)
+        self.map = None
 
         # continually updated info about robot's pose wrt odom
         self.posx = None
         self.posy = None
         self.angle = None
-
-        # useful transformation matrices for movement
-        self.mTo = np.array([[-1, 0, 1, 5], [0, -1, 0, 5], [0, 0, 1, 0], [0, 0, 0, 1]])  # odom to map
-        self.bTo = None  # odom to base_link
-        self.world = None  # if we do not use world in here, delete this later
-
-        self.id = Identifier()
-        self.lis = tf.TransformListener()
-        self.pose = {"x": 0, "y": 0, "z": 0}  # TODO : MERGE
-        self.pose_last_scan = {"x": 0, "y": 0, "z": 0}  # TODO : MERGE
-
         self.last_posx = None
         self.last_posy = None
         self.last_angle = None
 
-        self.time_last_scan = None  # TODO : MERGE
+        # useful transformation matrices for movement
+        self.mTo = np.array(
+            [[-1, 0, 1, START_X_MAP], [0, -1, 0, START_Y_MAP], [0, 0, 1, 0], [0, 0, 0, 1]])  # odom to map
+        self.bTo = None  # odom to base_link
+        self.world = None  # if we do not use world in here, delete this later
+
+        self.lis = tf.TransformListener()
         self.rcvr = None
+        self.id = Identifier()
+        self.time_last_scan = None
 
         rospy.sleep(SLEEP)
 
@@ -72,20 +75,6 @@ class Robot:
                            msg.info.origin)
         self.rcvr = Recovery(self.world)
 
-    def get_transform(self):
-        # get transformation from odom to base_link
-        (trans, rot) = self.lis.lookupTransform('robot_0/base_link', 'robot_0/odom', rospy.Time(0))
-
-        # get everything in regular matrix form
-        t = tf.transformations.translation_matrix(trans)
-        r = tf.transformations.quaternion_matrix(rot)
-        self.bTo = t.dot(r)
-
-    def move(self):
-        # TODO: add actual logic to this function
-        # TODO: get predicted x and z velocities from Predictor, combine with Identifier info to calculate move
-        pass
-
     def map_callback(self, msg):
         print("loading map")
         self.map = World(msg.data, msg.info.width, msg.info.height, msg.info.resolution, msg.header.frame_id,
@@ -94,41 +83,43 @@ class Robot:
         print(self.map.T)
 
     def laser_scan_callback(self, laser_scan_msg):
+
         curr_time = rospy.get_time()
         if self.time_last_scan is None or (curr_time - self.time_last_scan > 1 / SCAN_FREQ):
-            self.time_last_scan = curr_time
+            print "{}".format(''.join(['-' for _ in range(100)]))
 
             # print "Pose: ({}, {}, {})".format(show(self.posx), show(self.posy), show(self.angle))
             # print "LPos: ({}, {}, {})".format(show(self.last_posx), show(self.last_posy), show(self.last_angle))
-            movement_transform = None
             if self.posx is not None and self.last_posx is not None:
-                movement_transform = Robot.get_ref_change_xform(
-                    self.posx, self.posy, self.angle, self.last_posx, self.last_posy, self.last_angle)
+                self.id.blobify(laser_scan_msg)
+                self.id.classify(self.get_movement_transform())
 
             self.last_posx = self.posx
             self.last_posy = self.posy
             self.last_angle = self.angle
+            self.time_last_scan = curr_time
 
-            if self.posx is not None:
-                self.id.blobify(laser_scan_msg)
-            if self.posx is not None and self.last_posx is not None:
-                self.id.classify(movement_transform)
-
-            target_pos = self.get_target_pos()
+            target_pos = self.get_target_pos(frame="ODOM")
+            # print "Robot  @ ({}, {})".format(show(self.posx), show(self.posy))
             if target_pos is not None:
-                print "Target @ ({},{})".format(target_pos[0], target_pos[1])
+                print "Target @ ({},{})".format(show(target_pos[0]), show(target_pos[1]))
             else:
                 print "TARGET: Not found"
 
-    @staticmethod
-    def get_ref_change_xform(x1, y1, z1, x2, y2, z2):
-        sz1 = np.sin(z1)
-        cz1 = np.cos(z1)
-        mat1 = np.matrix([[cz1, -sz1, x1], [sz1, cz1, y1], [0, 0, 1]])
+    def move(self):
+        # TODO: add actual logic to this function
+        # TODO: get predicted x and z velocities from Predictor, combine with Identifier info to calculate move
+        pass
 
-        sz2 = np.sin(z2)
-        cz2 = np.cos(z2)
-        mat2 = np.matrix([[cz2, -sz2, x2], [sz2, cz2, y2], [0, 0, 1]])
+    def get_movement_transform(self):
+
+        sz1 = np.sin(self.angle)
+        cz1 = np.cos(self.angle)
+        mat1 = np.matrix([[cz1, -sz1, 0, self.posx], [sz1, cz1, 0, self.posy], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+        sz2 = np.sin(self.last_angle)
+        cz2 = np.cos(self.last_angle)
+        mat2 = np.matrix([[cz2, -sz2, 0, self.last_posx], [sz2, cz2, 0, self.last_posy], [0, 0, 1, 0], [0, 0, 0, 1]])
 
         return mat2.getI().dot(mat1)
 
@@ -144,25 +135,42 @@ class Robot:
             self.pub.publish(vel_msg)
             rate.sleep()
 
-    def get_target_pos(self):
+    def get_target_pos(self, frame):
 
         if self.id.target is None:
             return None
 
-        tx = self.id.target.mean[0]
-        ty = self.id.target.mean[0]
+        sz, cz = np.sin(self.angle), np.cos(self.angle)
 
-        sz = np.sin(self.angle)
-        cz = np.cos(self.angle)
+        x_base_scan, y_base_scan = self.id.target.mean[0], self.id.target.mean[1]
+        pos_base_scan = np.array([[x_base_scan], [y_base_scan], [0], [1]])
+        if frame == "BASE":
+            return float(pos_base_scan[0][0]), float(pos_base_scan[1][0])
+        # print "BASE : {}".format(show_pos(pos_base_scan))
 
-        t_pose = np.matrix([[cz, -sz, self.posx], [sz, cz, self.posy], [0, 0, 1]]).dot(np.array([[tx], [ty], [1]]))
+        trans_base_scan_to_odom = np.matrix(
+            [[cz, -sz, 0, self.posx], [sz, cz, 0, self.posy], [0, 0, 1, 0], [0, 0, 0, 1]])
+        pos_odom = trans_base_scan_to_odom.dot(pos_base_scan)
+        if frame == "ODOM":
+            return float(pos_odom[0][0]), float(pos_odom[1][0])
+        # print "ODOM : {}".format(show_pos(pos_odom))
 
-        return float(t_pose[0][0]), float(t_pose[1][0])
+        trans_odom_to_map = self.mTo
+        pos_map = trans_odom_to_map.dot(pos_odom)
+        if frame == "MAP":
+            return float(pos_map[0][0]), float(pos_map[1][0])
+        # print "MAP  : {}".format(show_pos(pos_map))
+
+        return None
+
+
+def show_pos(np_pos):
+    return show(float(np_pos[0][0])), show(float(np_pos[1][0]))
 
 
 class Identifier:
     # The minimum change in laser reading between adjacent angles, to consider them to be coming from different objects
-    THRESH_RADIALLY_SEPARATE = 2.0
+    THRESH_RADIALLY_SEPARATE = 0.5
 
     # The maximum distance a blob's mean can move to imply that it's stationary
     THRESH_BLOB_STATIC = 0.5
@@ -171,7 +179,7 @@ class Identifier:
     # This is not set as the minimum since it is difficult to match a blob with it's last scan
     # Hence, we compare with all blobs in the last scan and the ones that have a large difference are probably
     # Different blobs and the ones with a small distance, but greater than THRESH_BLOB_STATIC, are moving blobs
-    THRESH_BLOB_MOVEMENT = 2.0
+    THRESH_BLOB_MOVEMENT = 1.5
 
     # Below are the statuses based on distance from target
     STATUS_CLOSE = 1
@@ -196,7 +204,6 @@ class Identifier:
 
     def blobify(self, laser_scan_msg):
 
-        # TODO : blobs <- laser_msg
         amin = laser_scan_msg.angle_min  # Minimum angle of overall scan
         incr = laser_scan_msg.angle_increment  # Angle increment of overall scan
         arr = laser_scan_msg.ranges  # List of readings
@@ -226,38 +233,55 @@ class Identifier:
 
     def classify(self, movement_transform):
 
-        # TODO : target.pos, [obs] <- blobs
         # Pick out moving blobs
         obs = []
         target = None
-        shift_min_target = None
+        shift_min_target = 100
 
-        for blob_id, blob in self.blobs.items():
+        # print "\nBLOBS"
+        # print [b.show() for b in self.blobs.values()]
+        # print "\nLAST_BLOBS"
+        # print [b.show() for b in self.last_blobs.values()]
 
-            for last_blob_id, last_blob in self.last_blobs.items():
+        for last_blob_id, last_blob in self.last_blobs.items():
 
-                last_mean = np.array([[last_blob.mean[0]], [last_blob.mean[1]], [1]])
-                last_mean_now = movement_transform.dot(last_mean)
+            last_mean_now = movement_transform.getI().dot(
+                np.array([[last_blob.mean[0]], [last_blob.mean[1]], [0], [1]])
+            )
+
+            for blob_id, blob in self.blobs.items():
+
+                # TODO : invert mov trans?
+                # if last_blob_id == blob_id:
+                #     print "\nBLOB_IDs : {}, {}".format(last_blob_id, blob_id)
+                #     print "Last : ({}, {})".format(show(last_mean_now[0][0]), show(last_mean_now[1][0]))
+                #     print "Blob : ({}, {})".format(show(blob.mean[0]), show(blob.mean[1]))
+
                 dx = last_mean_now[0][0] - blob.mean[0]
                 dy = last_mean_now[1][0] - blob.mean[1]
                 shift = float(np.sqrt(dx * dx + dy * dy))
 
                 if shift < Identifier.THRESH_BLOB_STATIC:
+
+                    # print "STATIC"
                     obs.append(blob)
                     # print "obs id'd : {}".format(blob.show())
                     continue
 
                 elif shift < Identifier.THRESH_BLOB_MOVEMENT:
 
-                    if shift_min_target is None or shift < shift_min_target:
+                    if shift < shift_min_target:
                         target = blob
                         shift_min_target = shift
+                        # print "MOVE-NEW"
+                #     else:
+                #         print "MOVE-OLD"
+                # else:
+                #     print "NO MATCH"
 
         self.obs = obs
-        self.target = target
-
+        self.target = target  # in base_scan ref
         # TODO : target.lin_vel <- (target.pos, target.last_pos)
-        # TODO : return target{pos,vel}
 
     def status(self):
         if self.target is None:
@@ -276,7 +300,7 @@ class Identifier:
 class Blob:
     def __init__(self, id):
         self.id = id
-        self.arr = None  # [coordinates of indicent laser rays in bot frame]
+        self.arr = None  # [coordinates of incident laser rays in bot frame]
         self.mean = None  # [mean coordinate in bot frame]
 
     def add_point(self, point):
