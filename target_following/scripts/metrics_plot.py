ecfc0979c2f8615d82dead735555b9a84688b7ae
Sanket Joshi <sanketjoshi4@gmail.com>
2020-12-01 07:18:58 -0800

    Multiline and smooth graphs

--- target_following/scripts/metrics_plot.py
+++ target_following/scripts/metrics_plot.py
@@ -5,7 +5,7 @@ import sys
 import matplotlib.pyplot as plt
 
 
-def generate_graph():
+def generate_graph(cmd_vel_freq=1):
     """
     This reads a csv metrics file and generates graph for the same
     The file path needs to be supplied as the first argument while running this code
@@ -13,32 +13,35 @@ def generate_graph():
     """
 
     try:
-        file_name = sys.argv[1]
-        try:
-            # Read csv
-            with open(file_name) as csv_file:
-                output = [row for row in csv.reader(csv_file, delimiter=',')][1:]
-                csv_file.close()
+        i = 1
+        for file_name in sys.argv[1:]:
+            try:
+                # Read csv
+                with open(file_name) as csv_file:
+                    output = [row for row in csv.reader(csv_file, delimiter=',')][1:]
+                    csv_file.close()
 
-                # Prepare matplotlib graph
-                x1 = [d[0] for d in output]
-                y1 = [d[1] for d in output]
-                plt.plot(x1, y1, label="Dist")
-                plt.xlabel('time (sec)')
-                plt.ylabel('dist (m)')
-                plt.title('Target distance vs time')
-                plt.legend()
+                    # Prepare matplotlib graph
+                    x = [d[0] for i, d in enumerate(output) if i % cmd_vel_freq == 0]
+                    y = [d[1] for i, d in enumerate(output) if i % cmd_vel_freq == 0]
+                    plt.plot(x, y, label="Run {}".format(i))
+                    i += 1
 
-                # Export graph
-                export_name = file_name[:-4] + ".png"
-                plt.savefig(export_name)
-                plt.title('Target dist cs time')
-                print 'Exported dist graph ({})'.format(export_name)
-        except:
-            print "File not found ({})".format(file_name)
+            except:
+                print "File not found ({})".format(file_name)
+        plt.xlabel('time (sec)')
+        plt.ylabel('dist (m)')
+        plt.title('Target distance vs time')
+        plt.legend()
+
+        # Export graph
+        export_name = sys.argv[1][:-4] + ".png"
+        plt.savefig(export_name)
+        plt.title('Target dist cs time')
+        print 'Exported dist graph ({})'.format(export_name)
     except:
         print "Missing file name argument"
 
 
 if __name__ == "__main__":
-    generate_graph()
+    generate_graph(cmd_vel_freq=10)
